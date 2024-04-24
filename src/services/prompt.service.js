1a5a7e7fed1bd097e4546e32eac8d256a3033b2a
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-04-24 13:26:42 -0300

    Cambios y mejoras en la prompt de prompt.service de promptgeneration

--- src/services/prompt.service.js
+++ src/services/prompt.service.js
@@ -30,8 +30,12 @@ export class PromptServices {
     const hf = new HfInference(config.iaKey);
     const response = await hf.textGeneration({
       model: 'meta-llama/Meta-Llama-3-8B-Instruct',
-      parameters: {details: false, decoder_input_details: false, return_full_text: false, do_sample: false, temperature: 0.1},
-      inputs: `Your name is nogabot, you should use this data ${dataString} to answer the following question ${message}, You must read the question and If it's a no sense prompt Example: "adawada", another Example: "uiqiuq", etc, you must detect this patterns and you must return a answer saying that the prompt was not a question, Example: "Answer: Sorry, you must send a valid question", I want the answer with the following structure "Answer: "` ,
+      parameters: {details: false, decoder_input_details: false, return_full_text: false, do_sample: false, temperature: 0.1, },
+      inputs: `detect_incoherent_message("${message}"),Your name is nogabot, you should use this data ${dataString} to answer the following question ${message}, 
+      Once you have the prompt you must do the following steps:
+      Step 1: You must read the prompt and detect the prompt's language if the prompt doesn't belong to any language then you should say that the prompt was not a valid prompt, Example: "Answer: Sorry, you must send a valid prompt". If the prompt is valid you can move to step 2.
+      Step 2: You should return the answer to the question: ${message} with the following structure: "Answer: .", once you have done that you can move to step 3.
+      Step 3: Now you can return the answer, remember to use this data: ${dataString} to answer the question: ${message}` ,
     })
     const text = response.generated_text;
     const regex = /Answer: "(.*?)"/;
@@ -39,8 +43,8 @@ export class PromptServices {
     const match = text.match(regex);
     const match2 = text.match(regex2);
     
+    console.log({response, match2});
     const matchInput = match === null? match2.input.split('Answer: ')[1].split('\n')[0]: match[1] 
-    console.log({response, matchInput});
     return matchInput
   }
 
