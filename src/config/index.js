731a88de4027dde9028e84260b1ff361334fe6be
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-04-24 11:58:03 -0300

    Commit antes del pull de rama dev

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
-  dbURL: process.env.DB_URL,
-  accessToken: process.env.ACCESS_TOKEN
+  dbURL: process.env.MONGODB_URL,
+  accessToken: process.env.API_TOKEN
 };
 
