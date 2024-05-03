5716a7d82c5dab4482643daaee37bb369d3d39c7
lautarohelp <lautarotoledo391@gmail.com>
2024-05-03 15:06:22 -0300

    antes del merging

--- src/middlewares/verificationToken.js
+++ src/middlewares/verificationToken.js
@@ -2,15 +2,13 @@ import jwt from 'jsonwebtoken'
 import { config } from '../config/index.js'
 
 export function verificationToken (req, res, next) {
-  const token = req.headers.authorization
-  const tokenString = JSON.stringify(token)
-console.log({token});
+  const authorizationHeader = req.headers.authorization
+  const token = authorizationHeader.split(' ')[1];
   if (!token) {
     console.error('Unauthorized access. Token not provided1.')
     // const response = formatApiResponse({ data: null, status: 401, message: 'Unauthorized access. Token not provided.' })
     // return res.status(response.status).json(response)
   }
-
   try {
     const validToken = jwt.verify(token, config.jwtSecret);
     console.log({validToken});
