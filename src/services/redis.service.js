6e3e967ab00c3abc0e29fcbbc23385dd02c26579
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-05-15 15:25:46 -0300

    Cambios en prompt y añadida recursividad

--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -3,19 +3,19 @@ import { config } from "../config/index.js";
 const client = createClient({
     password: config.redisPassword,
     socket: {
-        host: 'redis-18030.c1.asia-northeast1-1.gce.redns.redis-cloud.com',
-        port: 18030
+        host: config.redisHost,
+        port: config.redisPort
     }
 });
 await client.connect()
 
 export class RedisServices {
-async createItem(value,key, res){
+async createItem(value,key){
     const response = await client.set(key, value, {EX: 86400})
     return response
 }
-async getItem(id, res){
-    const response = await client.get(id)
+async getItem(key){
+    const response = await client.get(key)
     return response
 }
 }
