86bcd5396a188011c05508a7c6d7a963a7af492d
lautarohelp <lautarotoledo391@gmail.com>
2024-05-17 19:23:57 -0300

    implementacion de login y acceso a informacion privada, modificacion de tokens

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import './db/database.js';
 import express from 'express';
 import cors from 'cors';
 
-import { infoRouter, } from './routes/info.js';
+import { infoRouter, } from './routes/public/info.js';
 import bodyParser from 'body-parser';
 import { config } from './config/index.js';
 import { tokenRouter } from './routes/tokens.js';
 import { redisRouter } from './routes/redisRouter.js';
+import { userRouter } from './routes/private/users.js';
+import { infoPrivateRouter } from './routes/private/infoPrivate.js';
 
 
 const app = express();
@@ -24,8 +26,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/api/v1', tokenRouter)
-app.use('/api/v1', infoRouter)
 app.use('/api/v1', redisRouter)
+app.use('/api/v1', infoRouter)
+app.use('/api/v1/private', userRouter)
+app.use('/api/v1/private', infoPrivateRouter)
 
 
 app.listen(PORT, () => {
--- src/middlewares/verificationToken.js
+++ src/middlewares/verificationToken.js
@@ -3,15 +3,32 @@ import { config } from '../config/index.js'
 
 export function verificationToken (req, res, next) {
   const token = req.headers.authorization
-  const parsedToken = token.split('Bearer ')[1]
   
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized access.' });
   }
+  const parsedToken = token.split('Bearer ')[1]
   try {
     const validToken = jwt.verify(parsedToken, config.jwtSecret);
     return validToken? next(): res.status(401).json({ message: 'Unauthorized access.' });
   } catch (error) {
     return res.status(401).json({ message: 'Unauthorized access.' });
+  }
+}
+
+export function isAdmin (req, res, next) {
+  const token = req.headers.authorization
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized access.' });
+  }
+  const parsedToken = token.split('Bearer ')[1]
+  try {
+    const validToken = jwt.verify(parsedToken, config.jwtSecret);
+    if (validToken.role === "admin" ) {
+      return next()
+    }
+    return res.status(401).json({ message: 'Unauthorized access.' });
+  } catch (error) {
+    return res.status(401).json({ message: 'Unauthorized in access.' });
   }
 }
