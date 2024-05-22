8c1b25220ff1ca6522dc4568e2e8cf6ffebc6c73
Lorenzo Peñalver <lpenalver@nogadev.com>
2024-05-22 16:05:40 -0300

    Cambio de route de whatsapp

--- src/routes/whatsapRoute.js
+++ src/routes/whatsapRoute.js
@@ -1,12 +1,18 @@
 import {Router} from 'express';
 // import { verifyToken, RecievedMsg } from '../controllers/whatsappcontrollers.js';
-import * as wc from '../../src/controllers/whatsappcontrollers.js';
+import {RecievedMsg,verifyToken} from '../../src/controllers/whatsappcontrollers.js';
 
 
 export const whatsappRouter = Router()
-whatsappRouter.get("/", wc.verifyToken)
+whatsappRouter.get("/", async (req,res)=> {
+    const res = verifyToken()
+    return res
+})
 
 // whatsappRouter.get("/", verifyToken)
 // whatsappRouter.post("/", RecievedMsg)
-whatsappRouter.post("/", wc.RecievedMsg)
+whatsappRouter.post("/", ()=>{
+    const res = RecievedMsg()
+    return res
+})
 
