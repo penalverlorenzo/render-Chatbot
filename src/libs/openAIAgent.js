d3cd07c631e1fcc03923145adce7c56b697fe745
Facundo <facundofigueroa789@gmail.com>
2024-06-18 14:58:21 -0300

    10_ Se limpia el código en openAIAgent y se le extrae la lógica de envío de mensajes a slack

--- src/libs/openAIAgent.js
+++ src/libs/openAIAgent.js
@@ -1,28 +1,15 @@
-import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import { createRetrieverTool } from "langchain/tools/retriever";
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { createToolCallingAgent } from "langchain/agents";
 import { AgentExecutor } from "langchain/agents";
 import { DynamicTool } from '@langchain/core/tools'
 import { HumanMessage } from "@langchain/core/messages";
-// import { ChatMessageHistory } from "@langchain/community/stores/message/in_memory";
-// import { BaseChatMessageHistory } from "@langchain/core/chat_history";
+
 import { RunnableWithMessageHistory } from "@langchain/core/runnables";
 import { InMemoryChatMessageHistory } from "@langchain/core/chat_history";
-// ----- Slack imports ----
-import { SlackServices } from "../services/slack.service.js";
-import { slackClient } from "./slack.js";
-import { config as configuracion } from "../config/index.js";
-const { slack: { channelId } } = configuracion;
 
-// ---- modelo ----
+// ---- models ----
 const model = new ChatOpenAI({ model: "gpt-3.5-turbo" });
-
-const slackServices = new SlackServices(slackClient);
 
 export class agent {
   constructor() {
@@ -30,41 +17,31 @@ export class agent {
     }
 }
 
-async agentCreation(message, identificador, contexto) {
+async agentCreation(message, identificador, contexto, sessionId) {
     try {
       const userMsg = [new HumanMessage({ content: message })]
-      const loader = new CheerioWebBaseLoader(
-        "https://docs.smith.langchain.com/overview" //! pasar link de la db para hacer un get a los identificadores
-      );
-      const docs = await loader.load();
-      const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 1000,
-        chunkOverlap: 200,
-      });
-      // const documents = await splitter.splitDocuments(docs);
-      const documents = [contexto.dataString.replaceAll('"', '').replaceAll('{', ' ').replaceAll('}', ' ')]
-      const vectorStore = await MemoryVectorStore.fromTexts(
-        documents,
-        [{id: 1}],
-        new OpenAIEmbeddings({ apiKey: "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" })
-      );
-      const retriever = vectorStore.asRetriever();
-      console.log({docs, retriever});
-      const retrieverTool = await createRetrieverTool(retriever, {
-        name: "nogadev_search",
-        description:
-          "Search for information about Nogadev. For any questions about Nogadev, you must use this tool!.",
-      });
-      const newTool = new DynamicTool({
-        name: "FOO",
-        description:
-          `Buaca en la informacion que te proporcione, si en la data esto existe ${identificador} ejecuta la funcion.`,
 
-        func: async () => await slackServices.postMessage({ channelId, message: message }),
+      const documents = contexto.dataString.replaceAll('"', '').replaceAll('{', ' ').replaceAll('}', ' ');
+
+      const sendMessagesToSlackTool = new DynamicTool({
+        name: "MessagesToSlack",
+        description:
+          `Debes utilizar esta herramienta siempre que el mensaje sea 234`,
+
+        func: () => console.log('ejecutanding'),
       })
-      const tools = [retrieverTool, newTool];
+      const tools = [sendMessagesToSlackTool];
+
+      const prompt1 = `
+                Responde el mensaje usando esta información: ${documents}.
+                Tarea: Toma el mensaje, y respondelo usando la información e idioma anteriormente mencionados.
+                En caso de que el mensaje no esté realcionado a la información, dejales saber que no estas diseñado para responder a eso.
+                Si te piden una broma/chiste, cuenta una corta relacionada a la programación que esté en el idioma indicado.`;
+      const prompt0 = "Eres Kike, un asistente IA dedicado a responder preguntas sobre Nogadev"
+
       const prompt = ChatPromptTemplate.fromMessages([
-        ["system", "You are a helpful assistant"],
+        ["system", prompt0],
+        ["system", prompt1,],
         ["placeholder", "{chat_history}"],
         ["human", "{input}"],
         ["placeholder", "{agent_scratchpad}"],
@@ -77,7 +54,6 @@ async agentCreation(message, identificador, contexto) {
         tools,
       });
 
-     
       const agentWithChatHistory = new RunnableWithMessageHistory({
         runnable: agentExecutor,
         getMessageHistory: async (sessionId) => {
@@ -98,7 +74,7 @@ async agentCreation(message, identificador, contexto) {
     };
     
       const res = await agentWithChatHistory.invoke({ input: message }, config);
-      // console.log({res});
+      console.log({res});
       return res.output
     } catch (error) {
       console.log({error});
@@ -108,6 +84,3 @@ async agentCreation(message, identificador, contexto) {
   }
 
 }
-
-
-
